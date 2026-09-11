d265d97fb297f4172d6bb6d30198ffb741e917e3
Hema2 <49586027+Hema2-official@users.noreply.github.com>
2026-09-11 18:51:51 +0200

    feat: implement history delete button

--- src/lib/components/nav-history.svelte
+++ src/lib/components/nav-history.svelte
@@ -2,13 +2,19 @@
 	import ChevronRightIcon from '@lucide/svelte/icons/chevron-right';
 	import * as Collapsible from '$lib/components/ui/collapsible';
 	import * as Sidebar from '$lib/components/ui/sidebar';
-	import { RotateCcwClockIcon, SquarePenIcon } from '@lucide/svelte';
+	import { RotateCcwClockIcon, SquarePenIcon, TrashIcon } from '@lucide/svelte';
 	import { historyDb } from '$lib/utils/history.svelte';
 	import { viewState } from '$lib/states/ViewState.svelte';
+	import { Button } from '$lib/components/ui/button';
 
 	let open = $state(true);
 
 	const allEntries = historyDb.getAll();
+
+	function handleDelete(id: number, e: MouseEvent) {
+		e.stopPropagation();
+		historyDb.deleteEntry(id);
+	}
 </script>
 
 <Sidebar.Group>
@@ -35,13 +41,23 @@
 						{/snippet}
 					</Collapsible.Trigger>
 					<Collapsible.Content>
-						<Sidebar.MenuSub>
+						<Sidebar.MenuSub class="mr-1! pr-1!">
 							{#each allEntries.current as historyEntry (historyEntry.id)}
 								{#if historyEntry.id !== undefined}
 									{@const id = historyEntry.id}
 									<Sidebar.MenuSubItem class="cursor-default">
-										<Sidebar.MenuSubButton onclick={() => viewState.openHistory(id)}>
-											<span>{historyEntry.query.trim()}</span>
+										<Sidebar.MenuSubButton
+											onclick={() => viewState.openHistory(id)}
+											class="flex justify-between gap-2"
+										>
+											<span class="truncate">{historyEntry.query.trim()}</span>
+											<Button
+												variant="ghost"
+												size="icon-xs"
+												class="hidden group-hover/menu-sub-item:inline-flex hover:bg-destructive/20! hover:text-destructive!"
+												onclick={(e) => handleDelete(id, e)}
+												><TrashIcon class="size-3.5" />
+											</Button>
 										</Sidebar.MenuSubButton>
 									</Sidebar.MenuSubItem>
 								{/if}
